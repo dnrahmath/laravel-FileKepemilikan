c7684406e3d50e96a9c4c54e5416be864a682f09
Rahmat <dn.rahmath@gmail.com>
2021-05-04 18:53:57 +0700

    upload by model

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,7 +51,7 @@ class AuthController extends Controller
             return response()->json($response, 422);
         }
         
-        $user = User::create([
+        $user = User::create([          //----------------------------------------------
             'name' => $request['name'],
             'email' => $request['email'],
             'password' => Hash::make($request['password']),
